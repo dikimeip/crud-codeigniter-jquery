71dac80390d6587f1c491ab9eadc0c692c98dd33
dikimeip <pm159145@gmail.com>
2020-01-01 09:21:56 +0700

    memampilkan data di table

--- application/views/dasboard.php
+++ application/views/dasboard.php
@@ -17,16 +17,31 @@
 					<th>AKSI</th>
 				</tr>
 			</thead>
-			<tbody id="data"></tbody>
+			<tbody id="body"></tbody>
 		</table>	
 	</div>
 	
 </div>
 
-
-
-
 <script type="text/javascript" src="<?php echo base_url('asset/bootstrap/js/jquery.js') ?>"></script>
+<script>
+	onload()
+	function onload() {
+		$.ajax({
+			type:'POST',
+			url : '<?php echo base_url('CrudController/showData') ?>',
+			dataType : 'json',
+			success : function(data) {
+				var tampil = ""
+				for (var i = 0; i < data.data.length; i++) {
+					tampil += "<tr><td>"+data.data[i].nama+"</td><td>"+data.data[i].kelas+"</td><td>"+data.data[i].alamat+"</td><td><button class='btn btn-danger btn-sm'>HAPUS</button> <button class='btn btn-success btn-sm'>EDIT</button></td></tr>"
+				}
+				$("#body").html(tampil)
+			}
+		})
+	}
+</script>
+
 <script type="text/javascript" src="<?php echo base_url('asset/bootstrap/js/bootstrap.js') ?>"></script>
 </body>
 </html>
